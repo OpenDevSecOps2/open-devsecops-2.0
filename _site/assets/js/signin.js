711e1900b0d804b2bdff08114c65c6fd7b3f0a6c
BenLeland <blelan@uw.edu>
2025-03-07 01:10:13 -0800

    Added sign in panel

---
+++ _site/assets/js/signin.js
@@ -0,0 +1,8 @@
+let img = document.createElement("img")
+
+img.src = "/assets/images/signin.jpg"
+img.alt = "temp signin image"
+img.id = "signin"
+
+let main = document.getElementById("main-header")
+main.appendChild(img)
