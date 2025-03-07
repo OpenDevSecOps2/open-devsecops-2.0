3681cb48840aefbc60eea60935d09370195d12d3
BenLeland <blelan@uw.edu>
2025-03-07 01:52:39 -0800

    Added sign in placeholder

---
+++ _site/assets/js/signin.js
@@ -0,0 +1,8 @@
+let img = document.createElement("img")
+
+img.src = "/assets/images/signin.jpg"
+img.alt = "temp signin image"
+img.id = "signin"
+
+let main = document.getElementById("tocbot-toc")
+main.appendChild(img)
