5baded78d150e543d5f7f5108d0a46ec3e713515
BenLeland <blelan@uw.edu>
2025-05-12 15:20:18 -0700

    Attempt 6 to solve profile redirect

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -55,7 +55,7 @@ onAuthStateChanged(auth, (user) => {
         link.style.verticalAlign = 'middle';
         link.style.margin = '7px';
 
-        link.href = "{{ '/docs/other/profile.html' | relative_url }}";
+        link.href = "_site/docs/other/profile.html";
 
         link.appendChild(profileImage);
         ul.append(link);
